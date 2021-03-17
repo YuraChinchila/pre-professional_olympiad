8bfab274b4c4bef36f03201a9901680b37dd7b91
YuraChinchila <66523550+YuraChinchila@users.noreply.github.com>
2021-03-17 10:55:10 +0300

    Add files via upload

---
+++ php/check_user.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+	if (count($_POST) > 0) {
+		$login = htmlspecialchars(trim($_POST['login']));
+		$pass = htmlspecialchars(trim($_POST['pass']));
+		$pass = md5('jH1%cv2'.$pass);
+		$db = new PDO('sqlite:database.sqlite3');
+		$res = $db->query("SELECT id FROM users WHERE login='$login' AND pass='$pass'");
+		$elem = [];
+		while ($cur = $res->fetchObject()) {
+			$elem[] = $cur;
+		}
+		if (count($elem) > 0) {
+			$id = $elem[0]->id;
+			$_SESSION['id'] = $id;
+			header("Location: ../html/add_weight.html");
+		}
+	}
+?>
